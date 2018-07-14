4eb753e4a7babe2b9cbda90158129d2906849794
Km.Van <kmvan.com@gmail.com>
2018-07-14 22:19:12 +0800

    Add ES6 features
    Add ES6 features: const, arrow function.

--- src/index.ts
+++ src/index.ts
@@ -7,103 +7,117 @@
  * 把错误的数据转正
  * strip(0.09999999999999998)=0.1
  */
-function strip(num: number, precision = 12): number {
-  return +parseFloat(num.toPrecision(precision));
+const strip = (num: number, precision = 12): number => {
+  return +parseFloat(num.toPrecision(precision))
 }
 
 /**
  * Return digits length of a number
  * @param {*number} num Input number
  */
-function digitLength(num: number): number {
+const digitLength = (num: number): number => {
   // Get digit length of e
-  const eSplit = num.toString().split(/[eE]/);
-  const len = (eSplit[0].split('.')[1] || '').length - (+(eSplit[1] || 0));
-  return len > 0 ? len : 0;
+  const eSplit = num.toString().split(/[eE]/)
+  const len = (eSplit[0].split('.')[1] || '').length - +(eSplit[1] || 0)
+  return len > 0 ? len : 0
 }
 
 /**
  * 把小数转成整数，支持科学计数法。如果是小数则放大成整数
  * @param {*number} num 输入数
  */
-function float2Fixed(num: number): number {
+const float2Fixed = (num: number): number => {
   if (num.toString().indexOf('e') === -1) {
-    return Number(num.toString().replace('.', ''));
+    return Number(num.toString().replace('.', ''))
   }
-  const dLen = digitLength(num);
-  return dLen > 0 ? num * Math.pow(10, dLen) : num;
+  const dLen = digitLength(num)
+  return dLen > 0 ? num * Math.pow(10, dLen) : num
 }
 
 /**
  * 检测数字是否越界，如果越界给出提示
  * @param {*number} num 输入数
  */
-function checkBoundary(num: number) {
+const checkBoundary = (num: number) => {
   if (num > Number.MAX_SAFE_INTEGER || num < Number.MIN_SAFE_INTEGER) {
-    console.warn(`${num} is beyond boundary when transfer to integer, the results may not be accurate`);
+    console.warn(
+      `${num} is beyond boundary when transfer to integer, the results may not be accurate`
+    )
   }
 }
 
 /**
  * 精确乘法
  */
-function times(num1: number, num2: number, ...others: number[]): number {
+const times = (num1: number, num2: number, ...others: number[]): number => {
   if (others.length > 0) {
-    return times(times(num1, num2), others[0], ...others.slice(1));
+    return times(times(num1, num2), others[0], ...others.slice(1))
   }
-  const num1Changed = float2Fixed(num1);
-  const num2Changed = float2Fixed(num2);
-  const baseNum = digitLength(num1) + digitLength(num2);
-  const leftValue = num1Changed * num2Changed;
+  const num1Changed = float2Fixed(num1)
+  const num2Changed = float2Fixed(num2)
+  const baseNum = digitLength(num1) + digitLength(num2)
+  const leftValue = num1Changed * num2Changed
 
-  checkBoundary(leftValue);
+  checkBoundary(leftValue)
 
-  return leftValue / Math.pow(10, baseNum);
+  return leftValue / Math.pow(10, baseNum)
 }
 
 /**
  * 精确加法
  */
-function plus(num1: number, num2: number, ...others: number[]): number {
+const plus = (num1: number, num2: number, ...others: number[]): number => {
   if (others.length > 0) {
-    return plus(plus(num1, num2), others[0], ...others.slice(1));
+    return plus(plus(num1, num2), others[0], ...others.slice(1))
   }
-  const baseNum = Math.pow(10, Math.max(digitLength(num1), digitLength(num2)));
-  return (times(num1, baseNum) + times(num2, baseNum)) / baseNum;
+  const baseNum = Math.pow(10, Math.max(digitLength(num1), digitLength(num2)))
+  return (times(num1, baseNum) + times(num2, baseNum)) / baseNum
 }
 
 /**
  * 精确减法
  */
-function minus(num1: number, num2: number, ...others: number[]): number {
+const minus = (num1: number, num2: number, ...others: number[]): number => {
   if (others.length > 0) {
-    return minus(minus(num1, num2), others[0], ...others.slice(1));
+    return minus(minus(num1, num2), others[0], ...others.slice(1))
   }
-  const baseNum = Math.pow(10, Math.max(digitLength(num1), digitLength(num2)));
-  return (times(num1, baseNum) - times(num2, baseNum)) / baseNum;
+  const baseNum = Math.pow(10, Math.max(digitLength(num1), digitLength(num2)))
+  return (times(num1, baseNum) - times(num2, baseNum)) / baseNum
 }
 
 /**
  * 精确除法
  */
-function divide(num1: number, num2: number, ...others: number[]): number {
+const divide = (num1: number, num2: number, ...others: number[]): number => {
   if (others.length > 0) {
-    return divide(divide(num1, num2), others[0], ...others.slice(1));
+    return divide(divide(num1, num2), others[0], ...others.slice(1))
   }
-  const num1Changed = float2Fixed(num1);
-  const num2Changed = float2Fixed(num2);
-  checkBoundary(num1Changed);
-  checkBoundary(num2Changed);
-  return times((num1Changed / num2Changed), Math.pow(10, digitLength(num2) - digitLength(num1)));
+  const num1Changed = float2Fixed(num1)
+  const num2Changed = float2Fixed(num2)
+  checkBoundary(num1Changed)
+  checkBoundary(num2Changed)
+  return times(
+    num1Changed / num2Changed,
+    Math.pow(10, digitLength(num2) - digitLength(num1))
+  )
 }
 
 /**
  * 四舍五入
  */
-function round(num: number, ratio: number): number {
-  const base = Math.pow(10, ratio);
-  return divide(Math.round(times(num, base)), base);
+const round = (num: number, ratio: number): number => {
+  const base = Math.pow(10, ratio)
+  return divide(Math.round(times(num, base)), base)
 }
 
-export { strip, plus, minus, times, divide, round, digitLength, float2Fixed };
-export default { strip, plus, minus, times, divide, round, digitLength, float2Fixed };
+export { strip, plus, minus, times, divide, round, digitLength, float2Fixed }
+export default {
+  strip,
+  plus,
+  minus,
+  times,
+  divide,
+  round,
+  digitLength,
+  float2Fixed,
+}
